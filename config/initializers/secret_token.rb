ca2416693bdbc04d9e4c8f4232b44211c1969763
Martin Acuna <meacuna@uc.cl>
2013-01-18 15:19:05 -0300

    Clean up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Letsjock::Application.config.secret_token = '55c5e31a9fb9d07a0dc18f03ade8a886598a21052a7e2f31a2b73be158a5a5c83a44505364d703c1a77b000f65afcfdce324ca21f7f16f61e07ba607e946719b'
