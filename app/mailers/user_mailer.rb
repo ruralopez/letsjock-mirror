59a5fa12abf19e839cc7c5d61b7523d3ca4e74bc
Martin Acuna <meacuna@uc.cl>
2013-05-14 01:47:13 -0400

    recuperacion password

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,4 +8,10 @@ class UserMailer < ActionMailer::Base
     @data = data
     mail(:to => "info@letsjock.com", :subject => "Contact form", :from => "letsjock@gmail.com")
   end
+
+  def new_password(user)
+    @user = user
+    mail(:to => user.email, :subject => "New password request", :from => "letsjock@gmail.com")
+  end
+
 end
