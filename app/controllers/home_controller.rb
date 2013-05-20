ae745bb5c67642aebc4d3faf25225d5927cda840
Cristhian Lopez <cplopez4@uc.cl>
2013-05-20 07:00:24 -0400

    Multiple changes Pre-launching

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,15 @@ class HomeController < ApplicationController
   def index
     if signed_in?
       redirect_to news_path
+    else
+      @user = User.new
+      @home = true
     end
   end
   
   def beta
     if signed_in?
       redirect_to news_path
-    else
-      @user = User.new
-      @home = true
     end
   end
 
---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,9 @@
+class SettingsController < ApplicationController
+
+  def index
+    if signed_in?
+      @user = current_user
+    end
+  end
+
+end
