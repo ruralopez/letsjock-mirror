39a00ed1c315e0ae669c63e1b1c3b82dc87963cb
William Tobar <wgtobar@gmail.com>
2013-06-06 18:25:46 -0400

    Incluyendo las news restantes y limpiando un poco el código

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,7 +2,10 @@ class FeedController < ApplicationController
 
   def index
     if signed_in?
-      ids = Subscription.joins(:publisher => :user).where("subscriptions.user_id = ? AND users.authentic_email = 1", current_user.id).collect(&:publisher_id)
+      publishers_ids = Subscription.joins(:publisher => :user).where("subscriptions.user_id = ? AND users.authentic_email = 1", current_user.id).collect(&:publisher_id)
+      others_ids = Subscription.joins(:publisher).where("subscriptions.user_id = ? AND publishers.user_id IS NULL", current_user.id).collect(&:publisher_id)
+      ids = publishers_ids + others_ids
+      
       @news = Activity.all(:conditions => ["publisher_id IN (?)", ids], :order => "created_at desc", :limit => 50)
     else
       flash[:error] = "You must be logged in."
