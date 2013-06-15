a99528651d603c8cf297b2784fcc6f8a471767df
Martin Acuna <meacuna@uc.cl>
2013-06-14 22:00:46 -0400

    conflictos

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -34,11 +34,8 @@ class SessionsController < ApplicationController
       end
     else
       if params[:session]
-        user = User.find_by_email(params[:session][:email].downcase)
+        user = User.unscoped.find_by_email(params[:session][:email].downcase)
         @pass = params[:session][:password]
-      else
-        user = User.find_by_email(params[:email].downcase)
-        @pass = params[:password]
       end
       if user && user.authenticate(@pass)
         if user.authentic_email
--- config/routes.rb
+++ config/routes.rb
@@ -67,6 +67,9 @@ Letsjock::Application.routes.draw do
   match '/send_mail_auth' => 'users#send_mail_auth', :as => :send_mail_auth
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
+  match '/follow_letsjock' => 'users#follow_letsjock', :as => :follow_letsjock
+  match '/stadistics' => 'stats#index', :as => :stats
+  match '/tags' => 'tags#index', :as => :tags
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
