e4196abaa08d888b5396f8ddb9962272b4d0ea1e
Martin Acuna <meacuna@uc.cl>
2013-06-17 16:01:46 -0400

    fix omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'c6mnXQYkbB2ARMx2JIxAaw', 'dVPLjg3a50YeuIVjGgHudIYXcH1L8hg05RCnAZOFxI'
-  provider :facebook, '378016135633790', '21c5e4f60737fb99b2701541c5b26377', scope: "user_birthday", :client_options => {:ssl => {:verify => false}}
+  provider :facebook, '378016135633790', '21c5e4f60737fb99b2701541c5b26377', :client_options => {:ssl => {:verify => false}}
 end
