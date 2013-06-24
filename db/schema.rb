7efd7f50770df99729e0484e2ad2f517b28eb7cc
Cristhian Lopez <cplopez4@uc.cl>
2013-06-23 20:36:14 -0400

    (M)Agregar columna :certified en User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130622005614) do
+ActiveRecord::Schema.define(:version => 20130624003421) do
 
   create_table "activities", :force => true do |t|
     t.integer  "publisher_id"
@@ -351,6 +351,7 @@ ActiveRecord::Schema.define(:version => 20130622005614) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "address"
+    t.boolean  "certified"
   end
 
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
