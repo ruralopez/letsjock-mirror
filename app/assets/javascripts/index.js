600b406b50304267162ba8d50c19189ce36ba054
Martin Acuna <meacuna@uc.cl>
2013-06-24 14:36:32 -0400

    arreglo facebook login

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,10 +1,15 @@
 //= require validations
 
 $(function(){
+    $("#button-facebook-login").click(function(){
+        window.location.href='/auth/facebook';
+    });
+
     var newUserForm;
     newUserForm = $("#new_user");
     var loginForm;
-    loginForm = $(".loginForm");
+    loginForm = $(".form-login");
     newUserForm.validation();
     loginForm.validation();
+
 });
