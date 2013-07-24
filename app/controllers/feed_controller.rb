93b43c5193de4b0628123a0217e64bfcdb662875
Martin Acuna <meacuna@uc.cl>
2013-07-24 16:15:39 -0400

    (B) news endless page

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,5 +1,5 @@
 class FeedController < ApplicationController
-
+  require 'will_paginate/array'
   def index
     if signed_in?
       # Si viene con un redirect
@@ -13,7 +13,7 @@ class FeedController < ApplicationController
       others_ids = Subscription.joins(:publisher).where("subscriptions.user_id = ? AND publishers.user_id IS NULL", current_user.id).collect(&:publisher_id)
       ids = publishers_ids + others_ids
       
-      @news = Activity.all(:conditions => ["publisher_id IN (?)", ids], :order => "created_at desc", :limit => 50)
+      @news = Activity.all(:conditions => ["publisher_id IN (?)", ids], :order => "created_at desc").paginate(:page => params[:page], :per_page => 15)
     else
       flash[:error] = "You must be logged in."
       sign_out
