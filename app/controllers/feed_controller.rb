c1a1f15c7b2c69994dc970b1b6ce715fa55bbb17
William Tobar <wgtobar@gmail.com>
2013-07-29 18:39:44 -0400

    Agregando la parte de following como noticias grupales

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -14,16 +14,34 @@ class FeedController < ApplicationController
       ids = publishers_ids + others_ids
       
       # Activities Singulares solo mostrar la mas reciente por usuario [ids = 000]
-      singles_ids = Activity.select(%q{ publisher_id, MONTH(created_at) as mes, ( WEEK(created_at,5) - WEEK(DATE_SUB(created_at, INTERVAL DAYOFMONTH(created_at)-1 DAY),5)+1 ) as semana, MAX(id) as id })
-        .where("publisher_id IN (?)", ids)
-        .where("act_type = '000'")
-        .group(:publisher_id, :mes, :semana)
-        .collect(&:id)
-        
-      singles = Activity.where("id IN (?)", singles_ids).order("created_at desc").limit(50)
+      singles = Activity
+        .where(
+          "id IN (
+            SELECT id
+            FROM (
+              SELECT publisher_id, MONTH(created_at) as mes, ( WEEK(created_at,5) - WEEK(DATE_SUB(created_at, INTERVAL DAYOFMONTH(created_at)-1 DAY),5)+1 ) as semana,
+                MAX(id) as id
+              FROM `activities`
+              WHERE publisher_id IN (?) AND act_type = '000'
+              GROUP BY publisher_id, mes, semana
+            ) as d
+          )", ids)
+        .order("created_at desc")
+        .limit(50)
       
       # Activities grupales
-      # 1. Relationship: Caso followed (Si sigo a los dos solo debería mostrar la notificación de uno
+      # 1. Relationship: Caso followed (Si sigo a los dos solo debería mostrar la notificación de uno)
+      followed = Activity.find_by_sql( ["
+        SELECT a.*, d.users
+        FROM (
+          SELECT publisher_id, MONTH(created_at) as mes, ( WEEK(created_at,5) - WEEK(DATE_SUB(created_at, INTERVAL DAYOFMONTH(created_at)-1 DAY),5)+1 ) as semana,
+            MAX(id) as id, GROUP_CONCAT(user_id) as users
+          FROM `activities`
+          WHERE publisher_id IN (?) AND act_type = '010'
+          GROUP BY publisher_id, mes, semana
+        ) as d, activities a
+        WHERE d.id = a.id
+        ORDER BY created_at desc", ids] )
       
       
       # 2. Fotos y videos (p.e. Usuario publicó 2 fotos nuevas)
@@ -33,7 +51,7 @@ class FeedController < ApplicationController
       # Activities únicas siempre se muestran (p.e. Posts, eventos, Recommend y Certified) [ids = 001, 002, 003, 004, 031, 032, 033, 100, 101, 102, 202]
       uniques = Activity.all(:conditions => ["publisher_id IN (?) AND act_type IN ('001', '002', '003', '004', '031', '032', '033', '100', '101', '102', '202')", ids], :order => "created_at desc", :limit => 50)
       
-      @news = ( singles + uniques ).sort_by(&:created_at).reverse
+      @news = ( singles + followed + uniques ).sort_by(&:created_at).reverse
       # master
       # @news = Activity.all(:conditions => ["publisher_id IN (?)", ids], :order => "created_at desc").paginate(:page => params[:page], :per_page => 15)
     else
