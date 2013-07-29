285cd572065a73b4b89fcaba39368adcbaae4c18
Martin Acuna <meacuna@uc.cl>
2013-07-29 16:31:12 -0400

    (R)arreglado el signo mas del edit profile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require jquery-ui
-//= require jquery.purr
 //= require best_in_place
 //= require users
 //= require bootstrap
